f099e30dacd3625f697e98c7c0ae4db3f41562a0
Joe McCann <joseph.isaac@gmail.com>
2011-03-31 22:50:06 -0500

    Futzing with the layout of the list items.

--- app/views/Templates.js
+++ app/views/Templates.js
@@ -1,4 +1,7 @@
 // Let's define some template helpers to display our data.
 teagrams.views.imgTpl = function () {
-    return "<img class='shot-image' width='120' height='120' src='http://i.tinysrc.mobi/286/{thumbnail_url}' />";
+    return "<div class='item-wrap'> " +
+    "<img class='teagram-image' width='120' height='120' src='http://i.tinysrc.mobi/286/{thumbnail_url}' />" +
+    "<div class='copy-wrap'><h2>: <span class='username'>{username}</span></h2></div>" +
+    "</div>";
 };

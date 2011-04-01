5434d2088b929ed189305e84167277072364a5ed
Joe McCann <joseph.isaac@gmail.com>
2011-04-01 10:40:53 -0500

    Added custom font.

--- app/views/Templates.js
+++ app/views/Templates.js
@@ -2,6 +2,6 @@
 teagrams.views.imgTpl = function () {
     return "<div class='item-wrap'> " +
     "<img class='teagram-image' width='120' height='120' src='http://i.tinysrc.mobi/286/{thumbnail_url}' />" +
-    "<div class='copy-wrap'><h2>: <span class='username'>{username}</span></h2></div>" +
+    "<div class='copy-wrap'><h2><span class='username'>{username}</span></h2></div>" +
     "</div>";
 };
